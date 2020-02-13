4ed49a0a7d438f60e3580aab0033e915d5667dcf
guangjun <547652008@qq.com>
2020-02-13 16:21:05 +0800

    docs: 增加打点注释

--- app/controller/logger.js
+++ app/controller/logger.js
@@ -7,9 +7,15 @@ class Con extends Controller {
   async log() {
     const { ctx } = this;
     ctx.body = 'success';
-    // ctx.query._timestamp = new Date().getTime();
-    // ctx.query._timestr = new Date().toString();
     ctx.logger.info(JSON.stringify(ctx.query));
+    /** 打点基础数据
+     * {
+     *    _appName: 'douban-group-filter', // 应用名
+     *    _event: 'userInputField', // 事件名称
+     *    _userId: 'xxxx',
+     *    _platform: '', // h5 | pc | xcx
+     * }
+     */
   }
 }
 
